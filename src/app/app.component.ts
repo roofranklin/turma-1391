2bdbe715d6b8aeaa7f04dd2cf8844d3a07ab46a7
Roosevelt Franklin <roosevelt.franklin@chatguru.com.br>
2025-08-09 12:34:58 -0300

    exercicios aula 01 e 02 e refatoração do service provider para usar http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
 
 @Component({
@@ -7,7 +7,7 @@ import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
-  imports: [RouterOutlet, ShoppingCartComponent]
+  imports: [RouterModule, ShoppingCartComponent]
 })
 export class AppComponent {
   title = 'minha-loja';
